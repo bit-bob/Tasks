ea2cda13974e6447f0c822412ed6e4ad8639ceb4
Helina <helina.berhane@outlook.com>
2023-08-09 15:30:12 +0100

    add tasks and lists of tasks with names

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 
 
+class Task():
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class Tasks():
+
+    _tasks = []
+
+    def addTask(self, task: Task) -> None:
+        self._tasks.append(task)
+
+    def __str__(self) -> str:
+        if self._tasks:
+            return ", ".join([str(t) for t in self._tasks])
+        return ""
+
+
 if __name__ == "__main__":
-    print("beep")
+    a = Task("beep")
+    b = Task("boop")
+
+    t = Tasks()
+    t.addTask(a)
+    t.addTask(b)
+
+    print(a)
+    print(b)
+    print(t)
