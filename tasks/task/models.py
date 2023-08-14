967353a9f1c45eff99d15b782d20a42ed1ff9797
Helina <helina.berhane@outlook.com>
2023-08-14 17:49:58 +0100

    add due to the task model

--- tasks/task/models.py
+++ tasks/task/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
+
 class Task(models.Model):
-  name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    due = models.DateField(null=True)
