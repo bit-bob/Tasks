691d87d014457e8b53c5d76bfe63074ada7c6588
Helina <helina.berhane@outlook.com>
2023-08-14 17:38:20 +0100

    add a html template

--- tasks/task/views.py
+++ tasks/task/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def task(request):
-    return HttpResponse("Hello world!")
+  template = loader.get_template('hello_world.html')
+  return HttpResponse(template.render())
