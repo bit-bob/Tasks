903eed914a92542c382e7511f32b13fe13d3aca6
Helina <helina.berhane@outlook.com>
2023-09-09 21:12:02 +0100

    archive django

--- tasks/task/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-
-def task(request):
-    template = loader.get_template('hello_world.html')
-    return HttpResponse(template.render())
