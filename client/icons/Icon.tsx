7f7ccec115f506d4f769cb112d636a37090a5dbe
Luke Harris <6074660+luk707@users.noreply.github.com>
2023-09-10 21:07:21 +0100

    Add Icons

--- client/icons/Icon.tsx
+++ client/icons/Icon.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import { ChevronDown } from "./ChevronDown";
+import { ChevronRight } from "./ChevronRight";
+import { Circle } from "./Circle";
 import { PauseFill } from "./PauseFill";
 import { PlayFill } from "./PlayFill";
 
 export const ICON_MAP = {
+  "chevron.down": ChevronDown,
+  "chevron.right": ChevronRight,
+  circle: Circle,
   "pause.fill": PauseFill,
   "play.fill": PlayFill,
 };
