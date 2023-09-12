594f391bd3c76c717c5213da44518bcef67051ef
Helina <helina.berhane@outlook.com>
2023-09-13 00:58:35 +0100

    add real tasks as demo tasks

--- router.py
+++ router.py
@@ -15,11 +15,30 @@ router = APIRouter(
 
 # Demo tasks
 demo_tasks = TaskList()
-demo_tasks.add_task('Demo task 1')
-demo_tasks.add_task('Demo task 2')
-demo_tasks.add_task('Demo task Three')
-demo_tasks.add_task('Demo task Wheee')
+demo_tasks.add_task('Create Tasks - front end - storybook components - create button')
+demo_tasks.add_task('Create Tasks - front end - storybook components - create form')
+demo_tasks.add_task('Create Tasks - back end - endpoints')
+demo_tasks.add_task('Create Tasks - front end - full implementation')
 
+demo_tasks.add_task('Complete Tasks - front end - storybook components - complete button')
+demo_tasks.add_task('Complete Tasks - front end - storybook components - task text changes')
+demo_tasks.add_task('Complete Tasks - front end - storybook components - move tasks to the bottom or hidden')
+demo_tasks.add_task('Complete Tasks - front end - storybook components - completed tasks page')
+demo_tasks.add_task('Complete Tasks - back end - endpoints')
+demo_tasks.add_task('Complete Tasks - front end - full implementation')
+
+demo_tasks.add_task('Delete Tasks - front end - storybook components - delete button')
+demo_tasks.add_task('Delete Tasks - back end - endpoints')
+demo_tasks.add_task('Delete Tasks - front end - full implementation')
+
+demo_tasks.add_task('Update Tasks - front end - storybook components - edit button')
+demo_tasks.add_task('Update Tasks - back end - endpoints')
+demo_tasks.add_task('Update Tasks - front end - full implementation')
+
+demo_tasks.add_task('Persisting Tasks')
+demo_tasks.add_task('Task Events')
+demo_tasks.add_task('Repeating Tasks')
+demo_tasks.add_task('Parent Tasks')
 
 # -- Tasks --
 # Read
