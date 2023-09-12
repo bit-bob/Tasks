2cd1f227ea21d6c93c6a65b0b6e5325e39f1e3df
Helina <helina.berhane@outlook.com>
2023-09-13 00:48:36 +0100

    get tasks in the front end from the api calls

--- client/app.tsx
+++ client/app.tsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GlobalStyles } from "./GlobalStyles";
 
-import { TaskListItem } from "./components/TaskListItem";
+import { TaskList } from "./components/TaskList";
+
+import { OpenAPI, TasksService, Task } from "../api-types";
+
+// TODO: fix to environment variable at build time
+OpenAPI.BASE = "http://localhost:3000";
 
 export const App = () => {
+  const [tasks, setTasks] = useState<Task[] | null>(null);
+
+  useEffect(() => {
+    let cancel = false;
+    TasksService.getTasks().then(response => response.tasks).then((tasks) => {
+      if (cancel) {
+        // this is run if the data is loaded but the component has been unmounted
+        return;
+      }
+      // this is run when the data is loaded and can be set on the component
+      setTasks(tasks);
+    });
+
+    // this is run when the component unmounts
+    return () => {
+      cancel = true
+    };
+  }, []);
+
   return (
     <>
       <GlobalStyles />
-      <TaskListItem
-        task={{
-          id: "0",
-          name: "Wash Dishes",
-        }}
-      />
+      {tasks === null ? "loading..." : <TaskList tasks={tasks}/>}
     </>
   );
 };
