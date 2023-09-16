55c86aa4ffe8a4b100fc1c648283c010764133ae
Luke Harris <6074660+luk707@users.noreply.github.com>
2023-09-16 16:15:55 +0100

    Use fontsource & local fonts

--- client/GlobalStyles.tsx
+++ client/GlobalStyles.tsx
@@ -1,5 +1,9 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+import "@fontsource/azeret-mono";
+import "@fontsource/comfortaa";
 
 export const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Azeret+Mono:wght@300&family=Comfortaa&family=Press+Start+2P&display=swap');
+${reset}
 `;
