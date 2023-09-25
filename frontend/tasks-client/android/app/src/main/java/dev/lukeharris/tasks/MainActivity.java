deaa8ea01a703f7172e5564e3f9419f5009b71c5
Luke Harris <6074660+luk707@users.noreply.github.com>
2023-09-25 22:09:30 +0100

    Configure capacitor status bar

--- frontend/tasks-client/android/app/src/main/java/dev/lukeharris/tasks/MainActivity.java
+++ frontend/tasks-client/android/app/src/main/java/dev/lukeharris/tasks/MainActivity.java
@@ -1,5 +1,15 @@
 package dev.lukeharris.tasks;
 
+import static android.view.View.OVER_SCROLL_NEVER;
+
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bridge.getWebView().setOverScrollMode(OVER_SCROLL_NEVER);
+    }
+}
